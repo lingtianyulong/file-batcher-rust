53e2c81949305fb3a32d26fe9098043b1d33444c
lingtianyulong <lingtianyulong@126.com>
2026-05-05 16:51:21 +0800

    1. users 中添加注释和创建时间、更新时间两个字段;

--- file-batcher/src-tauri/src/login/model.rs
+++ file-batcher/src-tauri/src/login/model.rs
@@ -3,7 +3,9 @@
  */
 #[allow(dead_code)]
 pub struct User {
-    pub id: i32,
-    pub username: String,
-    pub password: String,
+    pub id: i32,                // 用户ID
+    pub username: String,       // 用户名
+    pub password: String,       // 用户密码
+    pub create_time: String,    // 创建时间
+    pub update_time: String,    // 更新时间
 }
